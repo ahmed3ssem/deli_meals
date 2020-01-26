dfa660346fc5656d4d88fb553d572dd3bacb0f4d
a7med <a7med.assem@gmail.com>
2020-01-26 15:21:04 +0200

    Selecting Meals for a Chosen Category

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,3 +1,4 @@
+import 'package:deli_meals/dummy_data.dart';
 import 'package:flutter/material.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
@@ -7,13 +8,17 @@ class CategoryMealsScreen extends StatelessWidget {
   CategoryMealsScreen(this.categoryId,this.categoryTitle);
   @override
   Widget build(BuildContext context) {
+    final categoryMeals = DUMMY_MEALS.where((meal){
+      return meal.categories.contains(categoryId);
+    }).toList();
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle),
       ),
-      body: Center(
-        child: Text("Messi♥"),
-      ),
+      body: ListView.builder(itemBuilder: (context,index){
+        return Text(categoryMeals[index].title);
+
+      },itemCount: categoryMeals.length,),
     );
   }
 }
