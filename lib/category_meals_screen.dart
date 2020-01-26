5da9662b75dc96c38d6d70b23ee30eadabc687e3
a7med <a7med.assem@gmail.com>
2020-01-26 13:57:30 +0200

    Navigating to a New Page

---
+++ lib/category_meals_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Messi Page"),
+      ),
+      body: Center(
+        child: Text("Messi♥"),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'DeliMeals',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.pink,
         accentColor: Colors.amber,
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
         textTheme: ThemeData.light().textTheme.copyWith(
           body1: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
           body2: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
-          title: TextStyle(fontSize: 20,fontFamily: 'RobotoCondensed')
+          title: TextStyle(fontSize: 20,fontFamily: 'RobotoCondensed',fontWeight: FontWeight.bold)
         ),
       ),
       home: CategoriesScreen(),
