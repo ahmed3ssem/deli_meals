4b36832a9aa2fdd5be15ff7e9fe7fff4ec070879
a7med <a7med.assem@gmail.com>
2020-01-27 12:37:07 +0200

    Adding Custome Drwaer

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -1,5 +1,6 @@
 import 'package:deli_meals/screens/categories_screen.dart';
 import 'package:deli_meals/screens/favourites_screen.dart';
+import 'package:deli_meals/widget/main_drwaer.dart';
 import 'package:flutter/material.dart';
 
 class TabsScreen extends StatefulWidget {
@@ -27,6 +28,9 @@ class _TabsScreenState extends State<TabsScreen> {
       appBar: AppBar(
         title: Text(_pages[_selectPageIndex]['title']),
         centerTitle: true,
+      ),
+      drawer: Drawer(
+        child: MainDrawer(),
       ),
       body: _pages[_selectPageIndex]['Page'],
         bottomNavigationBar: BottomNavigationBar(
